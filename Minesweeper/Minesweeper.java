3fc4fa22513de6f91c9c41c5831d742a5f5af3f1
Chase <esmeraldarekz99@gmail.com>
2016-10-20 13:52:20 -0700

    save

--- Minesweeper/Minesweeper.java
+++ Minesweeper/Minesweeper.java
@@ -14,16 +14,40 @@ public class Minesweeper{
         for (int i =0; i<list.length; i++){
             list[i] = i +1;
         }
-  }
+   }
   public void printSquareMinesweeper(){
+      System.out.println(">>>>>>>>>>Welcome To Minesweeper<<<<<<<<");
       for (int i = 0; i<list.length; i++){
           if( i % 10 == 0 ){
               System.out.println(" ");
         }
-          if (i > 10 && i < 10){
-            System.out.println(" ");
+          if (i < 10 && i > 0){
+            System.out.print(" ");
         }
-        System.out.println(list[i]+ " ");
+        System.out.print(list[i]+ " ");
      }
   }
 }
+
+/**
+ * public class Minesweeper{
+    int [] list;
+    // list goes form 1- 100
+  public Minesweeper(int board){
+        list = new int [100];
+        for (int i =0; i<list.length; i++){
+            list[i] = i +1;
+        }
+   }
+  public void printSquareMinesweeper(){
+      System.out.println(">>>>>>>>>>Welcome To Minesweeper<<<<<<<<");
+      for (int i = 0; i<list.length; i++){
+          if( i % 10 == 0 ){
+              System.out.println(" ");
+        }
+          if (i < 10 && i > 0){
+            System.out.print(" ");
+        }
+        System.out.print(list[i]+ " ");
+     }
+ */
